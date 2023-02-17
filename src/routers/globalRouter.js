9b26ba63bdf937fa1f2ef219022f1034d89df829
EUNJI <eunji9180@naver.com>
2023-02-17 19:41:49 +0900

    230217 Newtube : #6.6~#6.14 init분리, video model

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router(); //라우터 변수를 생성
 
-globalRouter.get("/", trending); //라우터를 핸들러에 연결
+globalRouter.get("/", home); //라우터를 핸들러에 연결
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
